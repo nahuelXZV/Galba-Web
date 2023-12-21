120623952e090c38f25732af7882e52f0931433d
diegov67 <vargas.diego@ficct.uagrm.edu.bo>
2023-12-21 02:06:11 -0400

    reportes

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -3,66 +3,97 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use \setasign\Fpdi\Fpdi;
+use Codedge\Fpdf\Fpdf\Fpdf;
 use App\Models\Producto;
 
 class PDFController extends Controller
 {
+    protected $fpdf;
 
-    // Load data
-    function LoadData($file)
+    public function __construct()
     {
-        // Read file lines
-        $lines = file($file);
-        $data = array();
-        foreach($lines as $line)
-            $data[] = explode(';',trim($line));
-        return $data;
-    }
-
-    function ImprovedTable($header, $data)
-    {
-        // Column widths
-        $w = array(40, 35, 40, 45);
-        // Header
-        for($i=0;$i<count($header);$i++)
-            $this->Cell($w[$i],7,$header[$i],1,0,'C');
-        $this->Ln();
-        // Data
-        foreach($data as $row)
-        {
-            $this->Cell($w[0],6,$row[0],'LR');
-            $this->Cell($w[1],6,$row[1],'LR');
-            $this->Cell($w[2],6,number_format($row[2]),'LR',0,'R');
-            $this->Cell($w[3],6,number_format($row[3]),'LR',0,'R');
-            $this->Ln();
-        }
-        // Closing line
-        $this->Cell(array_sum($w),0,'','T');
+        $this->fpdf = new Fpdf;
     }
 
     public function generarPDF()
     {
-        // Instanciar FPDF
-        $pdf = new Fpdi();
+        $this->fpdf->AddPage();
+        // Cabecera de la tabla
+        $this->fpdf->SetFont('Arial', 'B', 12);
+        $this->fpdf->Cell(50, 10, 'Nombre', 1);
+        $this->fpdf->Cell(65, 10, 'Descripcion', 1);
+        $this->fpdf->Cell(25, 10, 'Cantidad', 1);
+        $this->fpdf->Cell(25, 10, 'Precio', 1);
+        $this->fpdf->Cell(30, 10, 'Monto', 1);
+        $this->fpdf->Ln();
 
-        // Column headings
-        $header = array('Nombre', 'Tamaño', 'Cantidad');
+        // Datos de la tabla (obtener datos de la base de datos)
+        $productos = Producto::getAllProductos(); // Obtener todos los productos
 
-        $data = Producto::getAllProductos();
+        $this->fpdf->SetFont('Arial', 'B', 9);
 
-        // Agregar una página
-        $pdf->AddPage();
+        foreach ($productos as $producto) {
+             // Establecer coordenadas actuales
+             $x = $this->fpdf->GetX();
+             $y = $this->fpdf->GetY();
+              
+             // Establecer el ancho de cada celda
+             $nombreWidth = 50;
+             $descripcionWidth = 70;
+             $cantidadWidth = 30;
+             $precioWidth = 30;
+ 
+              // Establecer el contenido para cada celda
+            $nombre = $producto->nombre;
+            $descripcion = $producto->descripcion;
+            $cantidad = $producto->cantidad;
+            $precio = $producto->precio;          
+ 
+             // Obtener la altura máxima necesaria para la fila actual
+            $alturaMaxima = max(
+                $this->getStringHeight($nombre, $nombreWidth),
+                $this->getStringHeight($descripcion, $descripcionWidth),
+                $this->getStringHeight($cantidad, $cantidadWidth),
+                $this->getStringHeight($precio, $precioWidth)
+            );
 
-        // Establecer fuente y tamaño del texto
-        $pdf->SetFont('Arial','',14);
+            // Dibujar las celdas con el alto máximo calculado
+            $this->fpdf->SetXY($x, $y);
+            $this->fpdf->MultiCell($nombreWidth, 10/2, utf8_decode($nombre), 1);
 
-        // Agregar texto
-        $pdf->ImprovedTable($header,$data);
+            $this->fpdf->SetXY($x + $nombreWidth, $y);
+            $this->fpdf->MultiCell($descripcionWidth, 10/2, utf8_decode($descripcion), 1);
 
-        // Salida del PDF (descarga o visualización)
-        $pdf->Output();
+            $this->fpdf->SetXY($x + $nombreWidth + $descripcionWidth, $y);
+            $this->fpdf->MultiCell($cantidadWidth, 10, utf8_decode($cantidad), 1);
+
+            $this->fpdf->SetXY($x + $nombreWidth + $descripcionWidth + $cantidadWidth, $y);
+            $this->fpdf->MultiCell($precioWidth, 10, utf8_decode($precio), 1);
+
+
+            // Mover a la siguiente fila
+            $this->fpdf->SetY($y + $alturaMaxima);
+        }
+
+        $this->fpdf->Output();
+
+        exit;
     }
 
+    function getStringHeight($text, $width) {
+        $fontFamily = "Arial";
+        $fontSize = 9;
+
+        // Obtenemos la anchura del texto en función de la fuente actual
+        $textWidth = $this->fpdf->GetStringWidth($text);
+
+        // Calculamos la cantidad de líneas que necesitará el texto en función del ancho de la celda
+        $numLines = ceil($textWidth / $width);
+
+        // Calculamos la altura aproximada multiplicando el número de líneas por la altura de la fuente
+        $height = $numLines * $fontSize;
+
+        return $height;
+    }
 
 }
